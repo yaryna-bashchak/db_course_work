89ce61498fa9146890bebf7ef22e013b56018ffe
yaryna-bashchak <y.bashchak.im11@kpi.ua>
2022-12-30 20:25:43 +0200

    change link

--- publish.sh
+++ publish.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:boldak/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f https://github.com/just-hoshinon/db_course_work.git master:gh-pages
+git push -f https://github.com/yaryna-bashchak/db_course_work.git master:gh-pages
 
 cd -
